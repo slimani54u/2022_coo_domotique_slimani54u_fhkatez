6fc00cc4eefb5faabfe6a0795c954241bfc6d0d2
Slimani yasser <yasser.slimani9@etu.univ-lorraine.fr>
2022-04-19 13:47:14 +0200

    2 eme push

---
+++ src/Telecommande.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class Telecommande {
+    ArrayList<Lampe> lampes;
+    public Telecommande() {
+        lampes = new ArrayList<Lampe>(0);
+    }
+    public void ajouterLampe (Lampe l){
+        if(l!=null) {
+            lampes.add(l);
+        }
+    }
+    public void activerLampe(int i){
+        lampes.get(i).allumer();
+    }
+    public void desactiverLampe(int i){
+        lampes.get(i).eteindre();
+    }
+    public void activerTout(){
+    for(Lampe l : lampes){
+        l.allumer();
+    }
+
+    }
+
+    @Test
+    public void TestTelecommande (){
+
+        //ajoute d'une lampe dans une telecommande vide
+        ArrayList<Lampe> t1 = new ArrayList<Lampe>(0);
+        Lampe l1 = new Lampe("lampe1");
+        t1.add(l1);
+        System . out . println ( t1 . contains ( l1 ));
+
+        //ajout d'une lampe à une télécommande avec 1 élément ;
+        ArrayList<Lampe> t2 = new ArrayList<Lampe>(1);
+        Lampe l2 = new Lampe("lampe2");
+        t2.add(l2);
+        System . out . println ( t2 . contains ( l2 ));
+
+        //activation d'une lampe existante en position 0 ;
+        ArrayList<Lampe> t3 = new ArrayList<Lampe>(2);
+        t3.add(l1);
+        t3.add(l2);
+        System.out.println(t3.get(0));
+        t3.get(0).allumer();
+        System.out.println(t3.get(0));
+        assertEquals("la lampe est pas allumer",l1.isAllume(),true);
+        t3.get(1).allumer();
+        assertEquals("la lampe est pas allumer",l2.isAllume(),true);
+
+
+        IndexOutOfBoundsException exception = assertThrows (
+                IndexOutOfBoundsException .class ,
+                 () -> { t3.get(4).allumer(); }
+         ) ;
+    }
+
+
+
+
+
+}
