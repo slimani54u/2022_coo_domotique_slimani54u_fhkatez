1155b9a586a48a280cdf82add42598e5000106f6
ZHANG Xin <zhang284u@etu.univ-lorraine.fr>
2022-04-25 14:32:48 +0200

    update

--- Zhang/src/Telecommande.java
+++ Zhang/src/Telecommande.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 
 public class Telecommande {
     ArrayList<Lampe> lampes;
+    ArrayList<Hifi>hifis;
 
-    public Telecommande(){
+    public Telecommande() {
         lampes = new ArrayList<Lampe>(0);
+        hifis = new ArrayList<Hifi>(0);
     }
 
     public void activerLampe(int i) {
@@ -18,7 +20,48 @@ public class Telecommande {
 
 
     public void ajouterLampe(Lampe l) {
-        lampes.add(l);
+        if (l != null) {
+            lampes.add(l);
+        }
     }
+
+    public String toString() {
+        String s = "";
+        for (Lampe lampe : lampes) {
+            s += lampe + "\n";
+        }
+
+        for(Hifi i : hifis){
+            s += i+"\n";
+        }
+        return s;
+    }
+
+    public void activerTout() {
+        for (Lampe l : lampes) {
+            l.allumer();
+        }
+    }
+
+    public void eteindretoushifi(){
+        for(Hifi i : hifis){
+            i.eteindre();
+        }
+    }
+
+    public void ajouthifi(Hifi i){
+        if(i != null){
+            hifis.add(i);
+        }
+    }
+
+    public void allmuercertainhifi(int i){
+        hifis.get(i).allumer();
+    }
+
+    public void eteindrecertainhifi(int i){
+        hifis.get(i).eteindre();
+    }
+
 
 }
